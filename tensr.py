b36916e8ca1996fc3bb465a11731c72e217b150e
AnishHemmady <anhemmady@gmail.com>
2017-08-03 18:20:45 -0400

    second_commit

--- tensr.py
+++ tensr.py
@@ -70,6 +70,8 @@ x=tf.placeholder(tf.float32,[None,img_flat])
 
 y_true=tf.placeholder(tf.int64,[None,no_of_classes])
 
+y_true_cls = tf.placeholder(tf.int64, [None])
+
 #2-D tensor representing weights
 weights=tf.Variable(tf.zeros([img_flat,no_of_classes]))
 
@@ -101,15 +103,91 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 #########################################################################################
 
 '''Here tensorflow graph generation code ends '''
+
+'''Actual working starts from below'''
 #########################################################################################
 session = tf.Session()
+#this below line intializes the tensorflow weights and biases.You need to initialize them otherwise error would be caused.
+session.run(tf.global_variables_initializer())
+batch_size=1000
 
+#actual working of code of above graph created starts from here
+def optimize(number_of_iterations):
+	for i in range(number_of_iterations):
+		x_batch, y_true_batch = data.train.next_batch(batch_size)
+		feed_dict_train = {x: x_batch,y_true: y_true_batch}
+		session.run(optimizer, feed_dict=feed_dict_train)
+		
+feed_dict_test = {x: data.test.images,y_true: data.test.labels,y_true_cls: data.test.cls}
+		
+def print_accuracy():
+    # Use TensorFlow to compute the accuracy.
+	acc = session.run(accuracy, feed_dict=feed_dict_test)
+    
+    # Print the accuracy.
+	print("Accuracy on test-set: {0:.1%}".format(acc))
+	
+	
+def plot_example_errors():
+	# Use TensorFlow to get a list of boolean values
+	# whether each test-image has been correctly classified,
+	# and a list for the predicted class of each image.
+	correct, cls_pred = session.run([correct_prediction, y_pred_cls],
+									feed_dict=feed_dict_test)
 
+	# Negate the boolean array.
+	incorrect = (correct == False)
 
+	# Get the images from the test-set that have been
+	# incorrectly classified.
+	images = data.test.images[incorrect]
 
+	# Get the predicted classes for those images.
+	cls_pred = cls_pred[incorrect]
 
+	# Get the true classes for those images.
+	cls_true = data.test.cls[incorrect]
 
+	# Plot the first 9 images.
+	plot_images(images=images[0:9],cls_true=cls_true[0:9],cls_pred=cls_pred[0:9])
+	
+	
+	
+	
+def plot_weights():
+	# Get the values for the weights from the TensorFlow variable.
+	w = session.run(weights)
 
+	# Get the lowest and highest values for the weights.
+	# This is used to correct the colour intensity across
+	# the images so they can be compared with each other.
+	w_min = np.min(w)
+	w_max = np.max(w)
 
+	# Create figure with 3x4 sub-plots,
+	# where the last 2 sub-plots are unused.
+	fig, axes = plt.subplots(3, 4)
+	fig.subplots_adjust(hspace=0.3, wspace=0.3)
 
+	for i, ax in enumerate(axes.flat):
+		# Only use the weights for the first 10 sub-plots.
+		if i<10:
+			# Get the weights for the i'th digit and reshape it.
+			# Note that w.shape == (img_size_flat, 10)
+			image = w[:, i].reshape(img_shape)
 
+			# Set the label for the sub-plot.
+			ax.set_xlabel("Weights: {0}".format(i))
+
+			# Plot the image.
+			ax.imshow(image, vmin=w_min, vmax=w_max, cmap='seismic')
+
+		# Remove ticks from each sub-plot.
+		ax.set_xticks([])
+		ax.set_yticks([])
+	plt.show()
+	
+optimize(30)
+print_accuracy()
+
+plot_example_errors()
